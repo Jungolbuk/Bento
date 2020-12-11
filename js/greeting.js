940f5c5b138cdfda8706c46907aff1f3ec7819a7
Jungolbuk <67849293+Jungolbuk@users.noreply.github.com>
2020-12-11 15:28:54 -0800

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -7,9 +7,9 @@ var name = 'Jaden';
 
 // Here you can change your greetings
 var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
-var gree3 = 'Good afternoon ';
-var gree4 = 'Good evening ';
+var gree2 = 'Why are you awake? ';
+var gree3 = 'Free time! Go do something! ';
+var gree4 = 'Grind time! ';
 var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
