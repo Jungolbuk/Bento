f0d4aef597750a09b1d1ab3f7f4c3944f00bbd26
Jungolbuk <67849293+Jungolbuk@users.noreply.github.com>
2020-12-11 15:31:40 -0800

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -8,16 +8,19 @@ var name = 'Jaden';
 // Here you can change your greetings
 var gree1 = 'Go to Sleep! ';
 var gree2 = 'Why are you awake? ';
-var gree3 = 'Free time! Go do something! ';
-var gree4 = 'Grind time! ';
+var gree3 = 'Free time! Go do something, ';
+var gree4 = 'Grind time, ';
 var gree5 = 'Good evening ';
 var gree6 = 'Good evening ';
 
+// Add comma in-between greeting and name
+var comma= ', ';
+
 // Define the hours of the greetings
 if (Hr >= 23 && Hr < 5) {
-    document.getElementById('greetings').innerText = gree1 + name;
+    document.getElementById('greetings').innerText = name + comma + gree1;
 } else if (Hr >= 6 && Hr < 12) {
-    document.getElementById('greetings').innerText = gree2 + name;
+    document.getElementById('greetings').innerText = name + comma + gree2;
 } else if (Hr >= 12 && Hr < 17) {
     document.getElementById('greetings').innerText = gree3 + name;
 } else if (Hr >= 17 && Hr < 23) {
